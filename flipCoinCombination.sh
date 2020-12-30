68610652799a00287f45c02886a6d27d78acdd92
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-30 19:14:03 +0530

    ADD.added doublet combination file

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,53 @@
 #! /bin/bash/ -x
-declare -A singletCombination
+declare -A doubletCombination
 heads=1
 tails=0
-headsCount=0
-tailsCount=0
+headsHeadsCount=0
+headsTailsCount=0
+tailsHeadsCount=0
+tailsTailsCount=0
 count=1
 read -p "enter a number how many times to flip a coin " multiple
+	function percentage() {
+		local count=$1
+		multiplication=$(( count*100 ))
+		percentage=$((  multiplication/multiple))
+		echo $percentage
+	}
+
 	while (( $count<=$multiple ))
 	do
 		flip=$(( RANDOM%2 ))
-		if (( $flip==$heads ))
+		flip1=$(( RANDOM%2 ))
+		if (( $flip==$heads && flip1==$heads))
 		then
-			((headsCount++))
-			singletCombination[ "flip $count" ]="H"
+			((headsHeadsCount++))
+			doubletCombination[ "doubleflip $count" ]="HH"
+		 elif (( $flip==$heads && flip1==$tails))
+                then
+                        ((headsTailsCount++))
+                        doubletCombination[ "doubleflip $count" ]="HT"
+		elif (( $flip==$tails && flip1==$heads))
+                then
+                        ((tailsHeadsCount++))
+                        doubletCombination[ "doubleflip $count" ]="TH"
 		else
-			 singletCombination[ "flip $count" ]="T"
-			((tailsCount++))
+                        ((tailsTailsCount++))
+                        doubletCombination[ "doubleflip $count" ]="TT"
 		fi
 		((count++))
 	done
-multiplication=$(( headsCount*100 ))
-percentage=$((  multiplication/multiple))
-echo "PERCENTAGE OF HEADS IN SINGLET COMBINATION "$percentage"%"
-multiplication=$(( tailsCount*100 ))
-percentage=$((  multiplication/multiple))
-echo "PERCENTAGE OF TAILS "$percentage"%"
-echo ${singletCombination[@]}
-echo ${!singletCombination[@]}
+percentage1="$( percentage $headsHeadsCount )"
+echo "PERCENTAGE OF (HH) IN DOUBLET COMBINATION "$percentage1"%"
+
+percentage1="$( percentage $headsTailsCount )"
+echo "PERCENTAGE OF (HT) IN DOUBLET COMBINATION "$percentage1"%"
+
+percentage1="$( percentage $tailsHeadsCount )"
+echo "PERCENTAGE OF (TH) IN DOUBLET COMBINATION "$percentage1"%"
+
+percentage1="$( percentage $tailsTailsCount )"
+echo "PERCENTAGE OF (TT) IN DOUBLET COMBINATION "$percentage1"%"
+
+echo ${doubletCombination[@]}
+echo ${!doubletCombination[@]}
