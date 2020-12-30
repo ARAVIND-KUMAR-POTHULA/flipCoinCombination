06483ff0ae4780250b8bc2c8677265c0906017e5
ARAVIND-KUMAR-POTHULA <pothulaaravind241998@gmail.com>
2020-12-30 18:44:46 +0530

    ADD. added a file which stores singlet combination and calculates percentage of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,29 @@
 #! /bin/bash/ -x
+declare -A singletCombination
 heads=1
 tails=0
-flip=$(( RANDOM%2 ))
-	if (( $flip==$heads ))
-	then
-		echo "HEADS IS WINNER"
-	else
-		 echo "TAILS IS WINNER"
-	fi
+headsCount=0
+tailsCount=0
+count=1
+read -p "enter a number how many times to flip a coin " multiple
+	while (( $count<=$multiple ))
+	do
+		flip=$(( RANDOM%2 ))
+		if (( $flip==$heads ))
+		then
+			((headsCount++))
+			singletCombination[ "flip $count" ]="H"
+		else
+			 singletCombination[ "flip $count" ]="T"
+			((tailsCount++))
+		fi
+		((count++))
+	done
+multiplication=$(( headsCount*100 ))
+percentage=$((  multiplication/multiple))
+echo "PERCENTAGE OF HEADS IN SINGLET COMBINATION "$percentage"%"
+multiplication=$(( tailsCount*100 ))
+percentage=$((  multiplication/multiple))
+echo "PERCENTAGE OF TAILS "$percentage"%"
+echo ${singletCombination[@]}
+echo ${!singletCombination[@]}
